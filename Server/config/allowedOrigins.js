1e2e450671e59fcb9ec6aa1131618be0e8843a7b
Junnu Kyrö <junnukyro@outlook.com>
2022-07-13 10:38:31 +0300

    Changes

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 //poista tämä sittenkun et enää tarvi ja laita herokun osoite tilalle
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000', 'https://verkkopankki.herokuapp.com'];
 
 module.exports = allowedOrigins;
