daa002e46b5b6031d5d22e52d53d73dd064f35fe
Junnu Kyrö <junnukyro@outlook.com>
2022-12-11 19:10:56 +0200

    Project finished

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 //allowed origins
-const allowedOrigins = ['http://localhost:3000', 'https://webbank.onrender.com', 'https://webbank.onrender.com/'];
+const allowedOrigins = ['http://localhost:10000', 'https://webbank.onrender.com', 'https://webbank.onrender.com/'];
 
 module.exports = allowedOrigins;
